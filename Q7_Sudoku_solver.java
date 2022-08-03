d48f58d1eaf408e57348b7988e447b4a203a6491
KomalKrishna12 <komalkrishna016@gmail.com>
2022-08-03 10:05:35 +0530

    sudoku solver

--- Q7_Sudoku_solver.java
+++ Q7_Sudoku_solver.java
@@ -20,13 +20,56 @@ public class Q7_Sudoku_solver {
         }
     }
 
-    public static void solve(char[][] board){
+    public static boolean solve(char[][] board){
 
         for(int row = 0; row < board.length; row++){
+
             for(int col = 0; col < board.length; col++){
                 
+                if(board[row][col] == '.'){
+
+                    for(char c = '1'; c <= '9'; c++){
+
+                        if(validate(row, col, c, board) == true){
+
+                            board[row][col] = c;
+
+                            if(solve(board) == true) return true;
+                            else board[row][col] = '.';
+
+                        }
+
+                    }
+
+                    return false;
+
+                }
+
             }
         }
+
+        return true;
+
+    }
+
+    public static boolean validate(int row, int col, char c, char[][] board){
+
+        for(int i = 0; i < 9; i++){
+
+            // check in same row 
+            if(board[row][i] == c) return false;
+
+            // check in same col
+            if(board[i][col] == c) return false;
+
+            // check in the 3*3 matrix
+            // firstly fins start row and col index for the given row and col
+            // we find it using (3 * (row / 3 ) + i / 3) and (3 * (col / 3) + i % 3)
+            if(board[3*(row/3)+(i/3)][3*(col/3)+(i%3)] == c) return false;
+
+        }
+
+        return true;
 
     }
 }
