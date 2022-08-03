be5f6e29b5c83c05b616eb1b90e2e1cf23639e2c
KomalKrishna12 <komalkrishna016@gmail.com>
2022-08-03 23:08:46 +0530

    rat in maze path

--- Q10_Rat_in_maze_problem_I.java
+++ Q10_Rat_in_maze_problem_I.java
@@ -6,9 +6,46 @@ public class Q10_Rat_in_maze_problem_I {
                      {1, 1, 0, 1}, 
                      {1, 1, 0, 0},
                      {0, 1, 1, 1}};
-           
-        ArrayList<String> ans = findPath(m, n);
-        System.out.println(ans);             
+         
+        // method 1
+        // in this we use four if statement for down, left, right , up
+        // ArrayList<String> ans = findPath(m, n);
+        // System.out.println(ans);             
+
+        // method 2
+        // in this we use single for loop for all four direction
+        ArrayList<String> ans = findPath2(m, n);
+        System.out.println(ans);
+    }
+
+    public static ArrayList<String> findPath2(int[][] board, int n){
+        ArrayList<String> ans = new ArrayList<>();
+        int[][] vis = new int[n][n];
+        int[] di = {+1, +0, +0, -1};
+        int[] dj = {+0, -1, +1, +0};
+        solve2(0, 0, board, vis, di, dj, ans, "");
+        return ans;
+    }
+
+    public static void solve2(int row, int col, int[][] board, int[][] vis, int[] di, int[] dj,
+    ArrayList<String> ans, String psf){
+        if(row == board.length - 1 && col == board.length - 1){
+            ans.add(psf);
+            return;
+        }
+
+        String str = "DLRU";
+        for(int i = 0; i < 4; i++){
+            int nexti = row + di[i];
+            int nextj = col + dj[i];
+
+            if(nexti >= 0 && nextj >= 0 && nexti < board.length && nextj < board.length && 
+            board[nexti][nextj] == 1 && vis[nexti][nextj] == 0){
+                vis[row][col] = 1;
+                solve2(nexti, nextj, board, vis, di, dj, ans, psf + str.charAt(i));
+                vis[row][col] = 0;
+            }
+        }
     }
 
     public static ArrayList<String> findPath(int[][] m, int n) {
