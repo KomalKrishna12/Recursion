e74faa8f09eeb955a59fb3514a9c202fc55a3cb8
KomalKrishna12 <komalkrishna016@gmail.com>
2022-08-05 19:26:35 +0530

    ..

--- Q5_Print_all_permutations_of_string_and_array.java
+++ Q5_Print_all_permutations_of_string_and_array.java
@@ -2,12 +2,13 @@ import java.util.ArrayList;
 
 public class Q5_Print_all_permutations_of_string_and_array {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3};
+        int[] nums = { 1, 2, 3 };
 
         // Approach 1
-        // if array contains n numbers then it have total n! permutations 
+        // if array contains n numbers then it have total n! permutations
         // use a boolean array to check which element is used
-        // when small array list size equal to array size add that into ans means we got one of the 
+        // when small array list size equal to array size add that into ans means we got
+        // one of the
         // permutation
 
         // ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
@@ -15,7 +16,7 @@ public class Q5_Print_all_permutations_of_string_and_array {
         // boolean[] map = new boolean[nums.length];
 
         // permutations(nums, sl, ans, map);
-        
+
         // System.out.println(ans);
 
         // Approah 2
@@ -30,41 +31,45 @@ public class Q5_Print_all_permutations_of_string_and_array {
         ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
         permuattions2(0, nums, ans);
         System.out.println(ans);
+        
     }
+   
 
     public static void permuattions2(int idx, int[] nums, ArrayList<ArrayList<Integer>> ans) {
-        if(idx == nums.length){
+        if (idx == nums.length) {
             ArrayList<Integer> ds = new ArrayList<>();
-            for(int item : nums) ds.add(item);
+            for (int item : nums)
+                ds.add(item);
             ans.add(new ArrayList<>(ds));
             return;
         }
 
-        for(int i = idx; i < nums.length; i++){
+        for (int i = idx; i < nums.length; i++) {
             swap(i, idx, nums);
             permuattions2(idx + 1, nums, ans);
             swap(i, idx, nums);
         }
     }
 
-    public static void swap(int i, int j, int[] nums){
+    public static void swap(int i, int j, int[] nums) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 
     public static void permutations(int[] nums, ArrayList<Integer> sl, ArrayList<ArrayList<Integer>> ans,
-    boolean[] map){
+            boolean[] map) {
 
-        if(sl.size() == nums.length){
+        if (sl.size() == nums.length) {
             ans.add(new ArrayList<>(sl));
             return;
         }
 
         // check all the index from 0 to end
-        // if that is false then we can add it into sl and mark it true and call for next
-        for(int i = 0; i < map.length; i++){
-            if(map[i] == false){
+        // if that is false then we can add it into sl and mark it true and call for
+        // next
+        for (int i = 0; i < map.length; i++) {
+            if (map[i] == false) {
                 map[i] = true;
                 sl.add(nums[i]);
                 permutations(nums, sl, ans, map);
@@ -73,4 +78,6 @@ public class Q5_Print_all_permutations_of_string_and_array {
             }
         }
     }
+
+    
 }
